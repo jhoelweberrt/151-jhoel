6cc5b6694da9b020500a7eed3bde0cde2cdbbb0e
samu-rammon <92813402+samu-rammon@users.noreply.github.com>
2021-10-20 19:38:53 -0300

    uptade ex013

--- javascript/ex013/converter.js
+++ javascript/ex013/converter.js
@@ -1,7 +1,13 @@
+let resultado = document.getElementById('resultado')
+let frase = document.getElementById('frase')
+
+function esconder(){
+    resultado.style.visibility = 'hidden'
+}
 function convert(){
     let n = Number(document.getElementById('n').value)
     let dolar = n / 5.60
-    let saida = document.getElementById('saida')
-    saida.innerHTML = `O valor de R$${n} é de USD$${dolar}`
+    frase.innerHTML = `O valor de R$${n.toFixed(2)} é de USD$${dolar.toFixed(2)}`
+    resultado.style.visibility = 'visible'
     return false
 }
