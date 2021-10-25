8151b84622ad32e3f4035d9dfa6bec5f14835e22
Jhoel Webert <90803349+jhoelweberrt@users.noreply.github.com>
2021-10-25 19:57:36 -0300

    ex015 e ex016

--- javascript/ex015/salar.js
+++ javascript/ex015/salar.js
@@ -1,16 +1,21 @@
-let frase = document.getElementById('frase')
-let res = document.getElementById('resultado')
+res = document.getElementById("saida")
+frase = document.getElementById("frase")
+res.style.visibility = "hidden"
 
+function fechar() {
+    res.style.visibility = "hidden"
+}
 
-function salario(){
-let nome = document.getElementById('nome').value
-let n = Number(document.getElementById('n').value)
-let aum = Number(document.getElementById('aument').value)
+function salario() {
+    let nome = document.getElementById("nome").value
+    let sal = Number(document.getElementById("sal").value)
+    let reaj = Number(document.getElementById("reaj").value)
 
-let aumento = n * aum / 100
+    let aum = sal * reaj / 100
+    let novo = sal + aum
 
-let novo = n + aumento
-frase.innerHTML = `O funcionário <strong>${nome}</strong> tinha o sálario de R$${n.toFixed(2)}. Com um aumento de ${aum}%, o salário vai aumentar R$${aumento.toFixed(2)} e vai passar a ser de <strong>R$${novo.toFixed(2)}</strong>.`
+    res.style.visibility = "visible"
+    frase.innerHTML = `O funcionário <strong>${nome}</strong> tinha o salário de R$${sal.toFixed(2)}. Com um aumento de ${reaj}%, o salário vai aumentar R$${aum.toFixed(2)} e vai passar a ser de <strong>R$${novo.toFixed(2)}</strong>`
 
-return false
+    return false
 }
