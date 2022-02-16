17ceeaa1a2b30f9fa5678abe6aef8650e239f674
Jhoel Webert <90803349+jhoelweberrt@users.noreply.github.com>
2022-02-16 20:50:34 -0300

    new update

--- javascript/ex013/converter.js
+++ javascript/ex013/converter.js
@@ -6,7 +6,7 @@ function esconder(){
 }
 function convert(){
     let n = Number(document.getElementById('n').value)
-    let dolar = n / 5.60
+    let dolar = n / 5.14
     frase.innerHTML = `O valor de R$${n.toFixed(2)} é de USD$${dolar.toFixed(2)}`
     resultado.style.visibility = 'visible'
     return false
